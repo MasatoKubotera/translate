907470a85f796b6fdcefde7ffa7b325c8024472d
Masato Kubotera <MasatoKubotera>
2020-12-30 20:02:54 +0900

    add rosrun en command

--- start.bash
+++ start.bash
@@ -2,6 +2,7 @@
 
 gnome-terminal -- bash -c "roscore; bash"
 gnome-terminal -- bash -c "rosrun translate ja.py; bash"
+gnome-terminal -- bash -c "rosrun translate en.py; bash"
 gnome-terminal -- bash -c "rosrun translate de.py; bash"
 gnome-terminal -- bash -c "rosrun translate es.py; bash"
 gnome-terminal -- bash -c "rosrun translate fr.py; bash"

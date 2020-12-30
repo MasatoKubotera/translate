eeae7cfc8380db7d4bad26e99e3cc80423b80bd3
Masato Kubotera <MasatoKubotera>
2020-12-30 02:38:11 -0800

    change to add rosrun codes

--- start.bash
+++ start.bash
@@ -2,4 +2,9 @@
 
 gnome-terminal -- bash -c "roscore; bash"
 gnome-terminal -- bash -c "rosrun translate ja.py; bash"
-gnome-terminal -- bash -c "rosrun translate en.py; bash"
+gnome-terminal -- bash -c "rosrun translate de.py; bash"
+gnome-terminal -- bash -c "rosrun translate es.py; bash"
+gnome-terminal -- bash -c "rosrun translate fr.py; bash"
+gnome-terminal -- bash -c "rosrun translate zh.py; bash"
+gnome-terminal -- bash -c "rosrun translate ko.py; bash"
+gnome-terminal -- bash -c "rosrun translate random.py; bash"
